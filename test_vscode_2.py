2df2dddd5026af4943ebf808761bb8a99cba437a
Andrea Alessandrini <andrea.alessandrini1992@gmail.com>
2020-11-03 16:57:38 +0100

    Created test_vscode_2.py file.

---
+++ test_vscode_2.py
@@ -0,0 +1,18 @@
+############################## CHECK VAR TYPE #################################
+num_int = 1
+num_dec = 2.0
+char = 'Pippo'
+lista = ['papete', 'zio']
+tupla = ('porco', 3)
+print(type(num_int),type(num_dec),type(char), type(lista), type(tupla))
+###############################################################################
+
+############################### IF STATEMENT ##################################
+booleano = False
+if booleano == True:
+    print('Vero!')
+elif booleano == False:
+    print('Falso!')
+else:
+    print('Qualquadra non cosa...')
+###############################################################################
